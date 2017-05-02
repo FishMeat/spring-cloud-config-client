ef972b2de5b809ef99c97bf5ad3ae343c4150de9
sggb <shangggb@joyowo.com>
2017-05-02 11:26:39 +0800

    增加eureka配置

--- src/main/java/com/fishmeat/Application.java
+++ src/main/java/com/fishmeat/Application.java
@@ -2,6 +2,7 @@ package com.fishmeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class Application {
 
     @RequestMapping("/")
