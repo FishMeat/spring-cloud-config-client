b8d1300c2780f8b7f553912b7085e926a290c130
sggb <shangggb@joyowo.com>
2017-05-03 10:04:13 +0800

    增加Hystrix Dashboard

--- src/main/java/com/fishmeat/Application.java
+++ src/main/java/com/fishmeat/Application.java
@@ -3,6 +3,7 @@ package com.fishmeat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@EnableHystrixDashboard
 public class Application {
 
     @RequestMapping("/")
