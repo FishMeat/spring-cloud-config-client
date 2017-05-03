f441a67c68678a883743765ff7bbb5f4da151281
sggb <shangggb@joyowo.com>
2017-05-03 15:32:25 +0800

    hystrix测试

--- src/main/java/com/fishmeat/Application.java
+++ src/main/java/com/fishmeat/Application.java
@@ -1,9 +1,12 @@
 package com.fishmeat;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableCircuitBreaker
 public class Application {
 
+    @Autowired
+    private Service service;
+
     @RequestMapping("/")
     public String home() {
-        return "Hello World!";
+        return service.hello();
+    }
+
+    @Bean
+    Service service() {
+        return new Service();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
---
+++ src/main/java/com/fishmeat/Service.java
@@ -0,0 +1,15 @@
+package com.fishmeat;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+/**
+ * @author sggb
+ * @since 03.05.2017
+ */
+public class Service {
+
+    @HystrixCommand
+    String hello() {
+        return "Hello World!";
+    }
+}
