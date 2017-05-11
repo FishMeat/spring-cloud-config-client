0e9c1053530fffb5c8a964247d4262db9635d8f8
sggb <shangggb@joyowo.com>
2017-05-11 15:45:45 +0800

    去除import

--- src/main/java/com/fishmeat/Application.java
+++ src/main/java/com/fishmeat/Application.java
@@ -1,6 +1,5 @@
 package com.fishmeat;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
